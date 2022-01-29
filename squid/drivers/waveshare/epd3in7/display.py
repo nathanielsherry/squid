9b8adf9bba30b94e429cf8a2fb28dd0fd92e87b6
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2022-01-28 23:42:59 -0500

    added waveshare 3.7 inch epaper driver

---
+++ squid/drivers/waveshare/epd3in7/display.py
@@ -0,0 +1,27 @@
+import cairo
+from . import driver
+
+epd = None
+def init(**kwargs):
+    global epd
+    epd = driver.EPD()
+    epd.Init_4Gray()
+    epd.Clear(0xFF)
+
+def cairo_to_pil(cairo_surface):
+    w = cairo_surface.get_width()
+    h = cairo_surface.get_height()
+    data = cairo_surface.get_data()
+    from PIL import Image
+    pil_image = Image.frombuffer("RGBA", (w, h), data, "raw", "RGBA", 0, 1)
+    return pil_image
+    
+#accepts a cairo surface matching the width and height of this display and draws it
+def show(surface):
+    pil_image = cairo_to_pil(surface)
+    epd.display_4Gray(epd.getbuffer_4Gray(pil_image))
+    return
+
+def get_surface(): return cairo.ImageSurface(cairo.Format.ARGB32, get_width(), get_height())
+def get_width(): return epd.width
+def get_height(): return epd.height
